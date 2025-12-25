11b985482bce4b312a4c5bcdab130a2eb45fc1de
Gedeon Guerra <netoguerra360@gmail.com>
2025-12-24 21:21:22 -0300

    adicionando cénarios de buscar produtos em massa de dados em lista

--- cypress/e2e/3-loja-ebac/podrutos.cy.js
+++ cypress/e2e/3-loja-ebac/podrutos.cy.js
@@ -3,31 +3,48 @@
 
 import produtosPage from "../../support/page-objects/produtos.page"
 
-describe ('Funcionalidade; Produtos', () => {
+describe('Funcionalidade; Produtos', () => {
 
-beforeEach(() => {
-    produtosPage.visitarUrl()
-})
+    beforeEach(() => {
+        produtosPage.visitarUrl()
+    })
 
 
-it('Deve selecionar um produto da lista', () => {
-    produtosPage.buscarProdutoLista('Abominable Hoodie')
-    cy.get('.woocommerce-tabs').should('contain', 'Descrição')
+    it('Deve selecionar um produto da lista', () => {
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
+        cy.get('.woocommerce-tabs').should('contain', 'Descrição')
 
-})
+    })
 
-it.only('Deve buscar um produto com sucesso', () => {
-    let produto = 'Zeppelin Yoga Pant'
-    produtosPage.buscarProduto(produto)
-    cy.get('.product_title').should('contain', produto)
-})
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    })
 
-it('Deve visitar a página do produto', () => {
-    
-})
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Zeppelin Yoga Pant')
+        cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant')
+    })
 
-it('Deve adicionar produto ao carrinho', () => {
-    
-})
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Aero Daily Fitness Tee')
+        produtosPage.addProdutoCarrinho('L', 'Black', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+    })
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[2].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[2].tamanho,
+                dados[2].cor,
+                dados[2].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)
+        })
+
+
+    })
 
 })
